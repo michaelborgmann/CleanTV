d7ab0896e1fb4473c964a3cb4cf5e3366dfb2c5f
Michael Borgmann <michaelborgmann@mailbox.org>
2020-03-26 12:15:35 +0100

    Initial Commit

---
+++ CleanTV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CleanTV
+//
+//  Created by Michael Borgmann on 26.03.20.
+//  Copyright © 2020 Michael Borgmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
